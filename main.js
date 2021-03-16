6777fe5768304bf2e1725e462b796f0fea9e7745
Star786-sus <77275680+Star786-sus@users.noreply.github.com>
2021-03-16 13:00:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -37,8 +37,13 @@ function gotResult(error, results)
     if( error )
     {
         console.log("error");
+    }
+    else
+    {
+        console.log(results);
         document.getElementById("result_object_name").innerHTML = results[0].label;
         document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
         
+
     }
 }
