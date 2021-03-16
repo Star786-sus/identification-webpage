6392761a6b5f4215fafed5aca3a84a498146b458
Star786-sus <77275680+Star786-sus@users.noreply.github.com>
2021-03-16 12:52:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+Webcam.set({
+ width: 350,
+ height: 350,
+ imageformat: 'png',
+ png_quality: 90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function takesnapshot()
+{
+    Webcam.snap(function(data_uri)
+    {
+        document.getElementById("result").innerHTML = '<img id="capture_image" src="'+data_uri+'"/>;';
+    });
+
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json', modelloaded);
+
+function modelloaded()
+{
+    console.log('model loaded');
+}
+
+function check()
+{
+    img = document.getElementById("capture_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if( error )
+    {
+        console.log("error");
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+        
+    }
+}
